cd949394ed09655b76840b8101c181f1cdbaa171
encorey <lukasz.jagodka@protonmail.com>
2019-12-23 22:51:22 +0100

    Modified /input endpoint

--- index.js
+++ index.js
@@ -2,35 +2,59 @@ const express = require('express')
 const app = express()
 const Link =require('./database/models').Link
 
+//EJS
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
-  // res.render('index.ejs')
   Link.findAll({
     attributes: ['link', 'shortlink']
   }).then(link => {
     res.render('index', {link});
   })
-    
-  
 })
  
 app.get('/input', function (req, res){
-  res.render('input.ejs')
+  res.render('input.ejs', { alert: false })
 })
 
 app.post('/input', function (req, res){
-  const link = req.body.link;
-  const slink = req.body.slink.trim()
-  Link.create({
-    link: link,
-    shortlink: slink
-  }).then(User => {
-    console.log(User)
-  }).catch(err=> console.log(err))
-  res.redirect('/')
+  let link = req.body.link;
+  let shortLink = req.body.slink;
+  let linkWithPrefix = link.indexOf('http') !== -1 ? link : `https://${link}`
+
+  if(shortLink == ''){
+    shortLink = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+  }
+
+  Link.findOne({
+    where: {
+      shortlink: shortLink
+    }
+  }).then(link => {
+    if (!link) {
+      Link.create({
+        link: linkWithPrefix,
+        shortlink: shortLink
+      }).then(() => {
+        res.redirect('/')
+      })
+    } else {
+      res.render('input', { alert: true})
+    }
+  }).catch(err => {
+    return res.render('input', { alert: true })
+  })
 })
 
+app.get('/:id', function (req, res){ 
+  Link.findOne({
+    where:{
+      shortlink: req.params.id
+    }
+  }).then(Link =>{
+    res.redirect(Link.link)
+  })
+})
 
 app.listen(3000)
